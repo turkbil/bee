ab5ad229ced9acba7332b8a546ac8ac083e103fa
Nurullah Okatan <nurullah@turkbilisim.com.tr>
2025-03-26 03:08:03 +0300

    Otomatik yükleme - 2025-03-26 03:08:03

--- Modules/ModuleManagement/routes/web.php
+++ Modules/ModuleManagement/routes/web.php
@@ -1,10 +1,9 @@
 <?php
-
 use Illuminate\Support\Facades\Route;
 use Modules\ModuleManagement\App\Http\Livewire\ModuleComponent;
 use Modules\ModuleManagement\App\Http\Livewire\ModuleManageComponent;
 
-Route::middleware(['web', 'auth'])
+Route::middleware(['web', 'auth', 'tenant', 'module.permission:modulemanagement,view'])
     ->prefix('admin')
     ->name('admin.')
     ->group(function () {
@@ -12,6 +11,8 @@ Route::middleware(['web', 'auth'])
             ->name('modulemanagement.')
             ->group(function () {
                 Route::get('/', ModuleComponent::class)->name('index');
-                Route::get('/manage/{id?}', ModuleManageComponent::class)->name('manage');
+                Route::get('/manage/{id?}', ModuleManageComponent::class)
+                    ->middleware('module.permission:modulemanagement,update')
+                    ->name('manage');
             });
     });

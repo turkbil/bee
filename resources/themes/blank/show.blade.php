29d1fe3376367e55074b0dbe94c52d924420541a
Nurullah Okatan <nurullah@turkbilisim.com.tr>
2025-04-07 00:33:34 +0300

    Otomatik yükleme - -07.04.2025- 00:33

--- resources/themes/blank/show.blade.php
+++ resources/themes/blank/show.blade.php
@@ -4,11 +4,15 @@
     <div>
         <h2>{{ $page->title }}</h2>
         
+        <div class="meta">
+            <span>Oluşturma: {{ $page->created_at->format('d.m.Y') }}</span>
+        </div>
+        
         <div class="page-content">
             {!! $page->body !!}
         </div>
         
-        <div>
+        <div class="page-actions">
             <a href="{{ route('pages.index') }}">← Tüm Sayfalar</a>
         </div>
     </div>

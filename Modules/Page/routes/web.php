cf571f7e514e9dddd6ace4c950f967be22a97a2d
Nurullah Okatan <nurullah@turkbilisim.com.tr>
2025-04-07 00:01:09 +0300

    Otomatik yükleme - -07.04.2025- 00:01

--- Modules/Page/routes/web.php
+++ Modules/Page/routes/web.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use Modules\Page\App\Http\Livewire\PageComponent;
 use Modules\Page\App\Http\Livewire\PageManageComponent;
+use Modules\Page\Http\Controllers\PageFrontController;
 
 Route::middleware(['web', 'auth', 'tenant'])
     ->prefix('admin')
@@ -18,4 +19,14 @@ Route::middleware(['web', 'auth', 'tenant'])
                     ->middleware('module.permission:page,update')
                     ->name('manage');
             });
+    });
+
+
+// Ön yüz rotaları
+Route::middleware(['web'])
+    ->name('pages.')
+    ->prefix('pages')
+    ->group(function () {
+        Route::get('/', [PageFrontController::class, 'index'])->name('index');
+        Route::get('/{slug}', [PageFrontController::class, 'show'])->name('show');
     });
---
+++ themes/blank/show.blade.php
@@ -0,0 +1,19 @@
+@extends('themes.blank.layouts.default')
+
+@section('content')
+    <div class="page-detail">
+        <h2>{{ $page->title }}</h2>
+        
+        <div class="page-meta">
+            <span>Oluşturulma: {{ $page->created_at->format('d.m.Y') }}</span>
+        </div>
+        
+        <div class="page-content">
+            {!! $page->body !!}
+        </div>
+        
+        <div class="page-actions">
+            <a href="{{ route('pages.index') }}" class="btn btn-back">← Tüm Sayfalar</a>
+        </div>
+    </div>
+@endsection

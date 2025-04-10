add5cf92bdc87f677bb09e8e1b937dff6b71cef5
Nurullah Okatan <nurullah@turkbilisim.com.tr>
2025-04-10 14:38:42 +0300

    Otomatik yükleme - -10.04.2025- 14:38

--- Modules/Studio/app/Services/StudioAssetService.php
+++ Modules/Studio/app/Services/StudioAssetService.php
@@ -17,11 +17,20 @@ class StudioAssetService
         if ($useCdn) {
             $output .= '<link rel="stylesheet" href="https://unpkg.com/grapesjs@0.21.8/dist/css/grapes.min.css">';
         } else {
-            $output .= '<link rel="stylesheet" href="' . asset('build-studio/resources/assets/css/grapes.min.css') . '">';
+            // Önce admin/libs/studio içinde ara
+            if (file_exists(public_path('admin/libs/studio/css/grapes.min.css'))) {
+                $output .= '<link rel="stylesheet" href="' . asset('admin/libs/studio/css/grapes.min.css') . '">';
+            } else {
+                $output .= '<link rel="stylesheet" href="' . asset('modules/studio/css/grapes.min.css') . '">';
+            }
         }
         
         // Özel CSS dosyaları
-        $output .= '<link rel="stylesheet" href="' . asset('build-studio/resources/assets/css/studio.css') . '">';
+        if (file_exists(public_path('admin/libs/studio/css/studio-editor.css'))) {
+            $output .= '<link rel="stylesheet" href="' . asset('admin/libs/studio/css/studio-editor.css') . '">';
+        } else if (file_exists(public_path('modules/studio/css/studio-editor.css'))) {
+            $output .= '<link rel="stylesheet" href="' . asset('modules/studio/css/studio-editor.css') . '">';
+        }
         
         return $output;
     }
@@ -44,23 +53,38 @@ class StudioAssetService
         if ($useCdn) {
             $output .= '<script src="https://unpkg.com/grapesjs@0.21.8/dist/grapes.min.js"></script>';
         } else {
-            $output .= '<script src="' . asset('build-studio/resources/assets/js/grapes.min.js') . '"></script>';
+            // Önce admin/libs/studio içinde ara
+            if (file_exists(public_path('admin/libs/studio/grapes.min.js'))) {
+                $output .= '<script src="' . asset('admin/libs/studio/grapes.min.js') . '"></script>';
+            } else {
+                $output .= '<script src="' . asset('modules/studio/js/grapes.min.js') . '"></script>';
+            }
         }
         
         // GrapesJS eklentileri
         $plugins = config('studio.editor.plugins', []);
+        
         foreach ($plugins as $plugin => $enabled) {
             if ($enabled) {
                 if ($useCdn) {
                     $output .= '<script src="https://unpkg.com/grapesjs-' . $plugin . '"></script>';
                 } else {
-                    $output .= '<script src="' . asset('build-studio/resources/assets/js/plugins/grapesjs-' . $plugin . '.min.js') . '"></script>';
+                    // Önce admin/libs/studio/plugins içinde ara
+                    if (file_exists(public_path('admin/libs/studio/plugins/grapesjs-' . $plugin . '.min.js'))) {
+                        $output .= '<script src="' . asset('admin/libs/studio/plugins/grapesjs-' . $plugin . '.min.js') . '"></script>';
+                    } else if (file_exists(public_path('modules/studio/js/plugins/grapesjs-' . $plugin . '.min.js'))) {
+                        $output .= '<script src="' . asset('modules/studio/js/plugins/grapesjs-' . $plugin . '.min.js') . '"></script>';
+                    }
                 }
             }
         }
         
         // Özel JS dosyaları
-        $output .= '<script src="' . asset('build-studio/resources/assets/js/studio.js') . '"></script>';
+        if (file_exists(public_path('admin/libs/studio/studio.js'))) {
+            $output .= '<script src="' . asset('admin/libs/studio/studio.js') . '"></script>';
+        } else if (file_exists(public_path('modules/studio/js/studio.js'))) {
+            $output .= '<script src="' . asset('modules/studio/js/studio.js') . '"></script>';
+        }
         
         return $output;
     }

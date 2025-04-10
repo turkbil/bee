ece8999775ce200aa9905947ec46a25c350e4f7e
Nurullah Okatan <nurullah@turkbilisim.com.tr>
2025-04-10 12:27:43 +0300

    Otomatik yükleme - -10.04.2025- 12:27

--- Modules/Studio/resources/views/layouts/editor.blade.php
+++ Modules/Studio/resources/views/layouts/editor.blade.php
@@ -14,7 +14,7 @@
     <link rel="stylesheet" href="{{ asset('admin/libs/fontawesome-pro@6.7.1/css/all.min.css') }}">
     
     <!-- GrapesJS CSS -->
-    <link rel="stylesheet" href="https://unpkg.com/grapesjs@0.21.8/dist/css/grapes.min.css">
+    <link rel="stylesheet" href="{{ asset('admin/libs/studio/css/grapes.min.css') }}">
     
     <style>
         html, body {
@@ -95,21 +95,21 @@
     </div>
     
     <!-- jQuery -->
-    <script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
+    <script src="{{ asset('admin/libs/jquery@3.7.1/jquery.min.js') }}"></script>
     
     <!-- Tabler JS -->
     <script src="{{ asset('admin/js/tabler.min.js') }}"></script>
     
     <!-- GrapesJS ve Eklentileri -->
-    <script src="https://unpkg.com/grapesjs@0.21.8/dist/grapes.min.js"></script>
-    <script src="https://unpkg.com/grapesjs-blocks-basic@1.0.1"></script>
-    <script src="https://unpkg.com/grapesjs-preset-webpage@1.0.2"></script>
-    <script src="https://unpkg.com/grapesjs-style-bg@1.0.5"></script>
-    <script src="https://unpkg.com/grapesjs-plugin-export@1.0.11"></script>
-    <script src="https://unpkg.com/grapesjs-plugin-forms@2.0.5"></script>
-    <script src="https://unpkg.com/grapesjs-custom-code@1.0.1"></script>
-    <script src="https://unpkg.com/grapesjs-touch@0.1.1"></script>
+    <script src="{{ asset('admin/libs/studio/js/grapesjs.min.js') }}"></script>
+    <script src="{{ asset('admin/libs/studio/js/grapesjs-blocks-basic.min.js') }}"></script>
+    <script src="{{ asset('admin/libs/studio/js/grapesjs-preset-webpage.min.js') }}"></script>
+    <script src="{{ asset('admin/libs/studio/js/grapesjs-style-bg.min.js') }}"></script>
+    <script src="{{ asset('admin/libs/studio/js/grapesjs-plugin-export.min.js') }}"></script>
+    <script src="{{ asset('admin/libs/studio/js/grapesjs-plugin-forms.min.js') }}"></script>
+    <script src="{{ asset('admin/libs/studio/js/grapesjs-custom-code.min.js') }}"></script>
+    <script src="{{ asset('admin/libs/studio/js/grapesjs-touch.min.js') }}"></script>
     
     @livewireScripts
 </body>
-</html>
+</html> 

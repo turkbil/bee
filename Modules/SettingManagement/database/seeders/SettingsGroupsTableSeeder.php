ceba8c760e22551c02d52ae41d9495907eef731c
Nurullah Okatan <nurullah@turkbilisim.com.tr>
2025-04-19 23:29:01 +0300

    Otomatik yükleme - -19.04.2025- 23:29

--- Modules/SettingManagement/database/seeders/SettingsGroupsTableSeeder.php
+++ Modules/SettingManagement/database/seeders/SettingsGroupsTableSeeder.php
@@ -11,35 +11,11 @@ class SettingsGroupsTableSeeder extends Seeder
     public function run(): void
     {
         $groups = [
-            // Ana Gruplar (önem sırasına göre)
             ['id' => 1, 'name' => 'Sistem', 'parent_id' => null, 'icon' => 'fas fa-cogs'],
             ['id' => 2, 'name' => 'Tenant', 'parent_id' => null, 'icon' => 'fas fa-building'],
             ['id' => 3, 'name' => 'Kullanıcı', 'parent_id' => null, 'icon' => 'fas fa-users'],
             ['id' => 4, 'name' => 'Modül', 'parent_id' => null, 'icon' => 'fas fa-puzzle-piece'],
-            ['id' => 5, 'name' => 'Tema', 'parent_id' => null, 'icon' => 'fas fa-paint-brush'],
-            ['id' => 6, 'name' => 'Eklenti', 'parent_id' => null, 'icon' => 'fas fa-plug'],
-            
-            // Alt Gruplar
-            // Sistem altında
-            ['id' => 7, 'name' => 'Site Ayarları', 'parent_id' => 1, 'icon' => 'fas fa-sliders-h'],
-            ['id' => 8, 'name' => 'İletişim Bilgileri', 'parent_id' => 1, 'icon' => 'fas fa-address-card'],
-            ['id' => 9, 'name' => 'Sosyal Medya Bilgileri', 'parent_id' => 1, 'icon' => 'fas fa-share-alt'],
-            ['id' => 10, 'name' => 'Örnekler', 'parent_id' => 1, 'icon' => 'fas fa-flask'],
-            
-            // Tenant altında
-            ['id' => 11, 'name' => 'Tenant Özellikleri', 'parent_id' => 2, 'icon' => 'fas fa-cog'],
-            ['id' => 12, 'name' => 'Tenant Özel Ayarları', 'parent_id' => 2, 'icon' => 'fas fa-sliders-h'],
-            
-            // Modül altında
-            ['id' => 13, 'name' => 'Sayfa', 'parent_id' => 4, 'icon' => 'fas fa-file'],
-            ['id' => 14, 'name' => 'Portfolyo', 'parent_id' => 4, 'icon' => 'fas fa-images'],
-            
-            // Tema altında
-            ['id' => 15, 'name' => 'Default Tema', 'parent_id' => 5, 'icon' => 'fas fa-palette'],
-            
-            // Kullanıcı altında
-            ['id' => 16, 'name' => 'Yönetici Ayarları', 'parent_id' => 3, 'icon' => 'fas fa-user-shield'],
-            ['id' => 17, 'name' => 'Kullanıcı Ayarları', 'parent_id' => 3, 'icon' => 'fas fa-user-cog'],
+            ['id' => 5, 'name' => 'Site', 'parent_id' => null, 'icon' => 'fas fa-globe'],
         ];
         
         foreach ($groups as $group) {

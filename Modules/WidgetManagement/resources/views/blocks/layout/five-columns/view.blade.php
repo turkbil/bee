4c20fbc9aa92da36574b06d53b7ec89364c36c1f
Nurullah Okatan <nurullah@turkbilisim.com.tr>
2025-05-10 23:40:34 +0300

    Otomatik yükleme - -10.05.2025- 23:40

--- Modules/WidgetManagement/resources/views/blocks/layout/five-columns/view.blade.php
+++ Modules/WidgetManagement/resources/views/blocks/layout/five-columns/view.blade.php
@@ -1,19 +1,23 @@
 <section class="container py-5">
     <div class="row">
-        <div class="col-lg-3 col-md-6">
+        <div class="col-6 col-md">
             <h3>Sütun 1</h3>
             <p>İçerik buraya gelecek.</p>
         </div>
-        <div class="col-lg-3 col-md-6">
+        <div class="col-6 col-md">
             <h3>Sütun 2</h3>
             <p>İçerik buraya gelecek.</p>
         </div>
-        <div class="col-lg-3 col-md-6">
+        <div class="col-6 col-md">
             <h3>Sütun 3</h3>
             <p>İçerik buraya gelecek.</p>
         </div>
-        <div class="col-lg-3 col-md-6">
+        <div class="col-6 col-md">
             <h3>Sütun 4</h3>
+            <p>İçerik buraya gelecek.</p>
+        </div>
+        <div class="col-12 col-md">
+            <h3>Sütun 5</h3>
             <p>İçerik buraya gelecek.</p>
         </div>
     </div>

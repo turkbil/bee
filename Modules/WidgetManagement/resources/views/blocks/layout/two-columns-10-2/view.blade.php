7e5e90475eae80ab3f6d6fa64d97709f926689cb
Nurullah Okatan <nurullah@turkbilisim.com.tr>
2025-05-10 21:21:07 +0300

    Otomatik yükleme - -10.05.2025- 21:21

---
+++ Modules/WidgetManagement/resources/views/blocks/layout/two-columns-10-2/view.blade.php
@@ -0,0 +1,12 @@
+<section class="container py-5">
+    <div class="row">
+        <div class="col-lg-10 col-md-9">
+            <h3>Maksimum Alan</h3>
+            <p>Bu alanda ana içerik bulunacaktır. Maksimum genişlikte bir alan olarak tasarlanmıştır.</p>
+        </div>
+        <div class="col-lg-2 col-md-3">
+            <h3>Mini Alan</h3>
+            <p>Bu alan dar menüler veya kısa bilgiler için uygundur. </p>
+        </div>
+    </div>
+</section>
---
+++ Modules/WidgetManagement/resources/views/blocks/layout/two-columns-9-3/view.blade.php
@@ -0,0 +1,12 @@
+<section class="container py-5">
+    <div class="row">
+        <div class="col-lg-9 col-md-8">
+            <h3>Geniş Alan</h3>
+            <p>Bu alanda ana içerik bulunacaktır. Çok geniş bir alan olarak tasarlanmıştır.</p>
+        </div>
+        <div class="col-lg-3 col-md-4">
+            <h3>Dar Alan</h3>
+            <p>Bu alanda yan içerik veya kısa bilgiler bulunabilir. </p>
+        </div>
+    </div>
+</section>

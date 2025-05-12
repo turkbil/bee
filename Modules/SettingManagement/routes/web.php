c58aebc43cfd449c9341c9c97d665ec12135bfb9
Nurullah Okatan <nurullah@turkbilisim.com.tr>
2025-05-12 14:41:42 +0300

    Otomatik yükleme - -12.05.2025- 14:41

--- Modules/SettingManagement/routes/web.php
+++ Modules/SettingManagement/routes/web.php
@@ -10,42 +10,46 @@ use Modules\SettingManagement\App\Http\Livewire\ValuesComponent;
 use Modules\SettingManagement\App\Http\Livewire\TenantSettingsComponent;
 
 Route::middleware(['web', 'auth', 'tenant'])
-    ->prefix('admin/settingmanagement')
-    ->name('admin.settingmanagement.')
+    ->prefix('admin')
+    ->name('admin.')
     ->group(function () {
-        Route::get('/', GroupListComponent::class)
-            ->middleware('module.permission:settingmanagement,view')
-            ->name('index');
-            
-        Route::get('/group/manage/{id?}', GroupManageComponent::class)
-            ->middleware('module.permission:settingmanagement,update')
-            ->name('group.manage');
-            
-        Route::get('/group/list', GroupListComponent::class)
-            ->middleware('module.permission:settingmanagement,view')
-            ->name('group.list');
-            
-        Route::get('/items/{group}', ItemListComponent::class)
-            ->middleware('module.permission:settingmanagement,view')
-            ->name('items');
-            
-        Route::get('/item/manage', ManageComponent::class)
-            ->middleware('module.permission:settingmanagement,create')
-            ->name('manage');
-            
-        Route::get('/item/manage/{id}', ManageComponent::class)
-            ->middleware('module.permission:settingmanagement,update')
-            ->name('manage.edit');
-            
-        Route::get('/value/{id}', TenantValueComponent::class)
-            ->middleware('module.permission:settingmanagement,update')
-            ->name('value');
-            
-        Route::get('/values/{group}', ValuesComponent::class)
-            ->middleware('module.permission:settingmanagement,view')
-            ->name('values');
-            
-        Route::get('/tenant/settings', TenantSettingsComponent::class)
-            ->middleware('module.permission:settingmanagement,update')
-            ->name('tenant.settings');
+        Route::prefix('settingmanagement')
+            ->name('settingmanagement.')
+            ->group(function () {
+                Route::get('/', GroupListComponent::class)
+                    ->middleware('module.permission:settingmanagement,view')
+                    ->name('index');
+                    
+                Route::get('/group/manage/{id?}', GroupManageComponent::class)
+                    ->middleware('module.permission:settingmanagement,update')
+                    ->name('group.manage');
+                    
+                Route::get('/group/list', GroupListComponent::class)
+                    ->middleware('module.permission:settingmanagement,view')
+                    ->name('group.list');
+                    
+                Route::get('/items/{group}', ItemListComponent::class)
+                    ->middleware('module.permission:settingmanagement,view')
+                    ->name('items');
+                    
+                Route::get('/item/manage', ManageComponent::class)
+                    ->middleware('module.permission:settingmanagement,create')
+                    ->name('manage');
+                    
+                Route::get('/item/manage/{id}', ManageComponent::class)
+                    ->middleware('module.permission:settingmanagement,update')
+                    ->name('manage.edit');
+                    
+                Route::get('/value/{id}', TenantValueComponent::class)
+                    ->middleware('module.permission:settingmanagement,update')
+                    ->name('value');
+                    
+                Route::get('/values/{group}', ValuesComponent::class)
+                    ->middleware('module.permission:settingmanagement,view')
+                    ->name('values');
+                    
+                Route::get('/tenant/settings', TenantSettingsComponent::class)
+                    ->middleware('module.permission:settingmanagement,update')
+                    ->name('tenant.settings');
+            });
     });

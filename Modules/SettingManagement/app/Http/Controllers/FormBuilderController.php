4b7a364d7d7dcbb8612b53b579aeff1d75025674
Nurullah Okatan <nurullah@turkbilisim.com.tr>
2025-05-18 01:08:13 +0300

    Otomatik yükleme - -18.05.2025- 01:08

--- Modules/SettingManagement/app/Http/Controllers/FormBuilderController.php
+++ Modules/SettingManagement/app/Http/Controllers/FormBuilderController.php
@@ -5,6 +5,7 @@ namespace Modules\SettingManagement\App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Routing\Controller;
 use Modules\SettingManagement\App\Models\SettingGroup;
+use Modules\SettingManagement\App\Models\Setting;
 
 class FormBuilderController extends Controller
 {
@@ -21,5 +22,28 @@ class FormBuilderController extends Controller
             'success' => true,
             'layout' => $group->layout
         ]);
+    }
+    
+    /**
+     * Grup için ayarların listesini getir
+     * 
+     * @param Request $request
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function getSettings(Request $request)
+    {
+        $groupId = $request->input('group');
+        
+        if (!$groupId) {
+            return response()->json([]);
+        }
+        
+        $settings = Setting::where('group_id', $groupId)
+            ->where('is_active', true)
+            ->select('id', 'label', 'key', 'type')
+            ->orderBy('sort_order', 'asc')
+            ->get();
+            
+        return response()->json($settings);
     }
 }

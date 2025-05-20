bd7d2d0af9e4520239956c27dfc768eedbe9be11
Nurullah Okatan <nurullah@turkbilisim.com.tr>
2025-05-20 18:59:15 +0300

    Otomatik temiz yükleme - -20.05.2025- 18:59

--- Modules/SettingManagement/resources/views/livewire/partials/form-elements/divider.blade.php
+++ Modules/SettingManagement/resources/views/livewire/partials/form-elements/divider.blade.php
@@ -1,7 +1,3 @@
-<div class="col-12">
-    <div class="card mb-3 w-100">
-        <div class="card-body p-2">
-            <hr class="my-1">
-        </div>
-    </div>
+<div class="col-12 mb-4">
+    <hr>
 </div>
--- Modules/SettingManagement/resources/views/livewire/partials/form-elements/spacer.blade.php
+++ Modules/SettingManagement/resources/views/livewire/partials/form-elements/spacer.blade.php
@@ -1,19 +1,7 @@
 <div class="col-12">
-    <div class="card mb-3 w-100">
-        <div class="card-header">
-            <div class="d-flex align-items-center justify-content-between">
-                <h3 class="card-title d-flex align-items-center">
-                    <i class="fas fa-arrows-alt-v me-2 text-primary"></i>
-                    Boşluk Elementi
-                </h3>
-            </div>
-        </div>
-        <div class="card-body">
-            @php
-                $height = isset($element['properties']['height']) ? $element['properties']['height'] : 20;
-            @endphp
-            
-            <div style="height: {{ $height }}px;"></div>
-        </div>
-    </div>
+    @php
+        $height = isset($element['properties']['height']) ? $element['properties']['height'] : 20;
+    @endphp
+    
+    <div style="height: {{ $height }}px;"></div>
 </div>

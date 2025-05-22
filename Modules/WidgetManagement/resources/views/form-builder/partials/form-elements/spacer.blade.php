76d54b14335ffba69971cfb41ec885d850284b67
Nurullah Okatan <nurullah@turkbilisim.com.tr>
2025-05-22 21:16:24 +0300

    Otomatik temiz yükleme - -22.05.2025- 21:16

--- Modules/WidgetManagement/resources/views/form-builder/partials/form-elements/spacer.blade.php
+++ Modules/WidgetManagement/resources/views/form-builder/partials/form-elements/spacer.blade.php
@@ -1,42 +1,9 @@
 @php
-    $fieldType = $element['type'] ?? 'spacer';
     $width = isset($element['properties']['width']) ? $element['properties']['width'] : 12;
     $height = isset($element['properties']['height']) ? $element['properties']['height'] : 20;
-    $visible = isset($element['properties']['visible_md']) && $element['properties']['visible_md'] ? 'd-none d-md-block' : '';
-    $responsiveHeight = isset($element['properties']['responsive_height']) && is_array($element['properties']['responsive_height']) ? $element['properties']['responsive_height'] : [];
-    $heightSm = $responsiveHeight['sm'] ?? $height;
-    $heightMd = $responsiveHeight['md'] ?? $height;
-    $heightLg = $responsiveHeight['lg'] ?? $height;
-    $heightXl = $responsiveHeight['xl'] ?? $height;
-    $backgroundColor = isset($element['properties']['background_color']) ? $element['properties']['background_color'] : 'transparent';
     $spacerId = 'spacer-' . uniqid();
 @endphp
 
-<div class="col-{{ $width }} {{ $visible }}" id="{{ $spacerId }}">
-    <div style="height: {{ $height }}px; background-color: {{ $backgroundColor }};"></div>
+<div class="col-{{ $width }}" id="{{ $spacerId }}">
+    <div style="height: {{ $height }}px;"></div>
 </div>
-
-@if(!empty($responsiveHeight))
-<style>
-    @media (min-width: 576px) {
-        #{{ $spacerId }} > div {
-            height: {{ $heightSm }}px !important;
-        }
-    }
-    @media (min-width: 768px) {
-        #{{ $spacerId }} > div {
-            height: {{ $heightMd }}px !important;
-        }
-    }
-    @media (min-width: 992px) {
-        #{{ $spacerId }} > div {
-            height: {{ $heightLg }}px !important;
-        }
-    }
-    @media (min-width: 1200px) {
-        #{{ $spacerId }} > div {
-            height: {{ $heightXl }}px !important;
-        }
-    }
-</style>
-@endif
--- Modules/WidgetManagement/resources/views/form-builder/partials/form-elements/tab_group.blade.php
+++ Modules/WidgetManagement/resources/views/form-builder/partials/form-elements/tab_group.blade.php
@@ -1,26 +1,10 @@
 @php
-    $fieldType = $element['type'] ?? 'tab_group';
     $fieldLabel = $element['label'] ?? 'Sekme Grubu';
     $width = isset($element['properties']['width']) ? $element['properties']['width'] : 12;
     $isSystem = isset($element['system']) && $element['system'];
     $tabId = 'tabs-' . Str::random(6);
-    $tabsData = [];
-    $marginBottom = isset($element['properties']['margin_bottom']) ? $element['properties']['margin_bottom'] : 4;
-    $fillSpace = isset($element['properties']['fill_space']) && $element['properties']['fill_space'];
     $tabStyle = isset($element['properties']['tab_style']) ? $element['properties']['tab_style'] : 'tabs';
-    $variant = isset($element['properties']['variant']) ? $element['properties']['variant'] : 'default';
-    $cardBg = isset($element['properties']['card_bg']) ? 'bg-' . $element['properties']['card_bg'] : '';
-    $headerBg = isset($element['properties']['header_bg']) ? 'bg-' . $element['properties']['header_bg'] : '';
-    $bodyBg = isset($element['properties']['body_bg']) ? 'bg-' . $element['properties']['body_bg'] : '';
-    $cardBorder = isset($element['properties']['card_border']) ? 'border-' . $element['properties']['card_border'] : '';
-    $cardShadow = isset($element['properties']['card_shadow']) ? 'shadow-' . $element['properties']['card_shadow'] : '';
-    $tabAlignment = isset($element['properties']['tab_alignment']) ? 'justify-content-' . $element['properties']['tab_alignment'] : '';
-    $animate = isset($element['properties']['animate']) && $element['properties']['animate'];
-    $iconPosition = isset($element['properties']['icon_position']) ? $element['properties']['icon_position'] : 'left';
-    $activeTab = isset($element['properties']['active_tab']) ? (int)$element['properties']['active_tab'] : 0;
-    $minHeight = isset($element['properties']['min_height']) ? 'min-height: ' . $element['properties']['min_height'] . 'px' : '';
-    $equalHeight = isset($element['properties']['equal_height']) && $element['properties']['equal_height'];
-    $rememberTab = isset($element['properties']['remember_tab']) && $element['properties']['remember_tab'];
+    $tabsData = [];
     
     // JSON verilerini doğru şekilde çek
     if (isset($element['properties']['tabs']) && is_array($element['properties']['tabs'])) {
@@ -34,13 +18,12 @@
     }
 @endphp
 
-<div class="col-{{ $width }} mb-{{ $marginBottom }}">
-    <div class="card {{ $fillSpace ? 'h-100' : '' }} {{ $cardBg }} {{ $cardBorder }} {{ $cardShadow }}">
-        <div class="card-header {{ $headerBg }}">
+<div class="col-{{ $width }} mb-3">
+    <div class="card">
+        <div class="card-header">
             <div class="d-flex align-items-center justify-content-between">
                 @if($fieldLabel)
-                <h3 class="card-title d-flex align-items-center">
-                    <i class="fas fa-folder-open me-2 text-primary"></i>
+                <h3 class="card-title">
                     {{ $fieldLabel }}
                     @if($isSystem)
                         <span class="badge bg-orange ms-2">Sistem</span>
@@ -49,24 +32,13 @@
                 @endif
             </div>
             
-            <ul class="nav nav-{{ $tabStyle }} {{ $variant === 'pills' ? 'nav-pills' : '' }} {{ $variant === 'underline' ? 'nav-bordered' : '' }} card-header-{{ $tabStyle }} mt-2 {{ $tabAlignment }}">
+            <ul class="nav nav-{{ $tabStyle }} card-header-{{ $tabStyle }} mt-2">
                 @forelse($tabsData as $index => $tab)
                     <li class="nav-item">
-                        <a href="#{{ $tabId }}-{{ $index }}" class="nav-link {{ $index === $activeTab ? 'active' : '' }}" 
+                        <a href="#{{ $tabId }}-{{ $index }}" class="nav-link {{ $index === 0 ? 'active' : '' }}" 
                            data-bs-toggle="tab" id="{{ $tabId }}-tab-{{ $index }}" role="tab" 
-                           aria-controls="{{ $tabId }}-{{ $index }}" aria-selected="{{ $index === $activeTab ? 'true' : 'false' }}">
-                            <span class="d-inline-flex align-items-center">
-                                @if(isset($tab['icon']) && $iconPosition === 'left')
-                                    <i class="{{ $tab['icon'] }} me-2"></i>
-                                @endif
-                                {{ $tab['title'] ?? 'Sekme ' . ($index + 1) }}
-                                @if(isset($tab['icon']) && $iconPosition === 'right')
-                                    <i class="{{ $tab['icon'] }} ms-2"></i>
-                                @endif
-                                @if(isset($tab['badge']))
-                                    <span class="badge bg-{{ $tab['badge_color'] ?? 'primary' }} ms-2">{{ $tab['badge'] }}</span>
-                                @endif
-                            </span>
+                           aria-controls="{{ $tabId }}-{{ $index }}" aria-selected="{{ $index === 0 ? 'true' : 'false' }}">
+                            {{ $tab['title'] ?? 'Sekme ' . ($index + 1) }}
                         </a>
                     </li>
                 @empty
@@ -80,17 +52,17 @@
             </ul>
         </div>
         
-        <div class="card-body {{ $fillSpace ? 'overflow-auto' : '' }} {{ $bodyBg }}" style="{{ $minHeight }}">
+        <div class="card-body">
             <div class="tab-content">
                 @forelse($tabsData as $index => $tab)
-                    <div class="tab-pane {{ $animate ? 'fade' : '' }} {{ $index === $activeTab ? 'active show' : '' }} {{ $equalHeight ? 'h-100' : '' }}" 
+                    <div class="tab-pane {{ $index === 0 ? 'active show' : '' }}" 
                          id="{{ $tabId }}-{{ $index }}" 
                          role="tabpanel" 
                          aria-labelledby="{{ $tabId }}-tab-{{ $index }}">
                         @if(isset($tab['content']))
                             {!! $tab['content'] !!}
                         @elseif(isset($tab['elements']) && is_array($tab['elements']))
-                            <div class="row h-100">
+                            <div class="row">
                                 @foreach($tab['elements'] as $tabElement)
                                     @php
                                         $elementType = $tabElement['type'] ?? 'text';
